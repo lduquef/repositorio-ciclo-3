4f7d763de2e6ffd30c987b55475888dcde10f343
Andres Lopez <landres945@gmail.com>
2021-09-23 23:38:44 -0500

    se cambia el tipo de escritura en ingles a español en el boton y el titulo del formulario

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
     <div className="App">
       <section className="login_Developer">
       <img class="avatar" src={Developer} alt="Logo_del_grupo"/>
-      <h1>Login here</h1>
+      <h1>Iniciar Sesion</h1>
       <form>
       <label for="nombre_usuario">Nombre de usuario</label>
       <input type="text" placeholder="Ingrese Nombre de Usuario"></input>
       <label for="Constraseña">Constraseña</label>
       <input type="password" placeholder="Ingrese su contraseña"></input>
-      <input type="submit" value="Log In"></input>
+      <input type="submit" value="Ingresar"></input>
       <a href="#">He perdido mi contraseña</a><br>
       </br>
       <a href="#">Aun no tengo cuenta empresarial</a>
