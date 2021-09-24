b398fc2795bf98275c641c4d018eb5ab9694d80b
Layonal A <elalvarez29@gmail.com>
2021-09-24 16:15:27 -0500

    Se sube el index.html conectado a react con las librerias y se diseña la plantila de todas las interfaz

---
+++ Gestion de usuarios/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Developer from "./img/developer.jpg";
+import FondoTrabajo from "./img/fondotrabajo.JPG";
+
+function App() {
+  return (
+    <div className="conten">
+    <div className="App">
+
+      <section className="login_Developer">
+      <img class="avatar" src={Developer} alt="Logo_del_grupo"/>
+     
+      <form>
+      
+      </form>
+      </section>
+    </div>
+    </div>
+  );
+}
+
+export default App;
