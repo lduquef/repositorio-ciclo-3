8da72ce4a1fef4186aebbf3c5085a32acbd081f2
leidy romero <85379392+leidyromero-14@users.noreply.github.com>
2021-09-28 18:39:22 -0500

    inicio maquetacion

--- src/pages/Gestion_ventas.jsx
+++ src/pages/Gestion_ventas.jsx
@@ -1,7 +1,12 @@
 import React  from "react";
+import "../Estilos/Style.css"
 const Gestion_ventas = () =>{
-    return(<div>
-insertar código
-    </div>  )
+    return(
+    <div classname="page2">ventas totales
+        <div className="page_ventas"> ventas 1
+    
+        </div> 
+    </div> 
+        )
 }
 export default Gestion_ventas
