945834a25d07bd772c702239985f3c3ef7cdd6fc
Maribelvh <maribelvh.12@gmail.com>
2021-09-27 21:21:56 -0500

    corrección error en texto

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Developer from "./img/developer.jpg";
 
 function App() {
   return (
-    <div className="conten">
+    
     <div className="App">
+      <div className="conten">
       <section className="login_Developer">
       <img class="avatar" src={Developer} alt="Logo_del_grupo"/>
       <h1>Iniciar Sesion</h1>
       <form>
       <label for="nombre_usuario">Nombre de usuario</label>
       <input type="text" placeholder="Ingrese Nombre de Usuario"></input>
-      <label for="Constraseña">Constraseña</label>
+      <label for="Constraseña">Contraseña</label>
       <input type="password" placeholder="Ingrese su contraseña"></input>
-      <input type="submit" value="Ingresar"></input>
+      <input type="submit" value="Ingresar"></input> 
+      <div class="foot-lnk">
+        <a href="#forgot">Registrarse</a>
+        </div>
       
 
       </form>
