e5674ac918d2f03ec7ae4b08b369e3f86793b751
Luis Santiago Duque Franco <78662656+lduquef@users.noreply.github.com>
2021-09-29 17:42:18 -0500

    arreglo local

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 const Header2 =() =>{
     return (
     <header>
-        <div className="fill">
-        </div>
         <li> <Link to="../../public/index.html"> <img className="avatar" src={Developer} />
         </Link> </li>
             
@@ -14,9 +12,10 @@ const Header2 =() =>{
           <li><a href="#">Navegacion</a>
           <ul>
             <li><Link to="/src/pages/Gestion_usuario.jsx"> Usuarios </Link> </li>
-            <li><a href="#">Gestion de Ventas</a></li>
-            <li><a href="#">Roles de Usario</a></li>
+            <li><Link to="/src/pages/Gestion_ventas.jsx"> Ventas </Link></li>
+            <li><Link to="/src/pages/Gestion_ventas.jsx"> productos </Link></li>
             <li><a href="#">Mas Ventas</a></li>
+            <br />
         </ul>
             </li>
             <li><a href="#">Contacto</a></li>
--- src/pages/Gestion_ventas.jsx
+++ src/pages/Gestion_ventas.jsx
@@ -1,11 +1,10 @@
 import React  from "react";
 import "../Estilos/Style.css"
-import pages_format from "../components/pages_format";
 const Gestion_ventas = () =>{
     return(
-        <pages_format>
-
-        </pages_format>
+        <div>
+    
+        </div>    
         )
 }
 export default Gestion_ventas
