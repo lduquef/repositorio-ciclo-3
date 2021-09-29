e163a92200580a6b0f9b7d33b6c1d5727cba4cc3
Luis Santiago Duque Franco <78662656+lduquef@users.noreply.github.com>
2021-09-28 19:03:01 -0500

    solución page ventas

--- src/pages/Gestion_ventas.jsx
+++ src/pages/Gestion_ventas.jsx
@@ -2,11 +2,9 @@ import React  from "react";
 import "../Estilos/Style.css"
 const Gestion_ventas = () =>{
     return(
-    <div classname="page2">ventas totales
         <div className="page_ventas"> ventas 1
     
         </div> 
-    </div> 
         )
 }
 export default Gestion_ventas
