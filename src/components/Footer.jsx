829f2882b765a3d90d95fcee9755017686816b6d
Luis Santiago Duque Franco <78662656+lduquef@users.noreply.github.com>
2021-09-28 21:44:55 -0500

    hola

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@
 const Footer = ()=>{
     return(
         <footer>
-        <p>pbx:54513621651651</p>
-        <p>email: devgroup@mail.com</p>
+          <div className="fill2">
 
+          </div>
+        <span>Teléfono:018000xx -   Dirección: Cra.xx #00 - 00 <br/> 
+        E-mail:developerGroup@xxxxxx.com </span>
         <ul class="social-icons">
           <li><a href="https://www.facebook.com/" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
           <li><a href="https://github.com/lduquef/repositorio-ciclo-3" class="social-icon"> <i class="fa fa-github"></i></a></li>
