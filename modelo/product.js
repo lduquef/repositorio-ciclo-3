3752018434b17c5e97726909ba12bb2175f2a5b3
Layonal <90285997+Layonal@users.noreply.github.com>
2021-10-19 21:35:44 -0500

    actialización id por código

--- modelo/product.js
+++ modelo/product.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const ProductSchema = Schema({
-    id: String,
+    codigo: String,
     nombre: String,
     precio:{ type :Number, default:0},
     estado: String
